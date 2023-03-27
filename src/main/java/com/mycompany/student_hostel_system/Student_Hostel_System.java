613db1a1eff9aaa92e57fdbb2c316eea2ad43382
Nahteee <ethantkz2522@gmail.com>
2023-03-27 14:04:25 +0800

    AHHH

---
+++ src/main/java/com/mycompany/student_hostel_system/Student_Hostel_System.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.student_hostel_system;
+
+/**
+ *
+ * @author User
+ */
+public class Student_Hostel_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
